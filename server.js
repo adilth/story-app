d1c3d7d84011ce2ca6abde163663714a294cd936
adil tahiri <rajaadil1995@gmail.com>
2022-08-08 01:45:39 +0100

    add database

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
-
+const morgon = require("morgon");
+const connectDB = require("./config/db");
 //load config
 require("dotenv").config({ path: "./config/config.env" });
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgon("dev"));
+}
 const PORT = process.env.PORT;
 
+connectDB();
 app.listen(
   PORT,
   console.log(`server is connect in ${process.env.NODE_ENV} mode ${PORT}`)
