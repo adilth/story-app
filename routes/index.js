7576bac73c100a09983dec74326bea17be6ebddd
adil tahiri <rajaadil1995@gmail.com>
2022-08-09 01:32:58 +0100

    add routes to app

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+//@desc  login Landing Page
+//@routes   GET /login
+
+router.get("/", (req, res) => {
+  res.render("login");
+});
+//@desc   dashboard Landing Page
+//@routes   GET /Dashboard
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+module.exports = router;
