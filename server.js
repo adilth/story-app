2fb8f8ac8888ffa9274ff6bbe8d223d44794fb2c
adil tahiri <rajaadil1995@gmail.com>
2022-08-09 01:34:13 +0100

    connect stuff to server

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
 const morgon = require("morgon");
+const { engine } = require("express-handlebars");
 const connectDB = require("./config/db");
+const index = require("./routes/index");
 //load config
 require("dotenv").config({ path: "./config/config.env" });
 
+//loaging
 if (process.env.NODE_ENV === "development") {
   app.use(morgon("dev"));
 }
+
+//handlebars
+app.engine("handlebars", engine({ extname: ".hbs" }));
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+// routes
+app.use("/", index);
 const PORT = process.env.PORT;
 
 connectDB();
