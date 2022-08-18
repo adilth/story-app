d4fe71f93680ac5dfee5579c9267a0721d080605
adil tahiri <rajaadil1995@gmail.com>
2022-08-18 21:33:22 +0100

    separete db from server

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
 const { engine } = require("express-handlebars");
 const methodOverride = require("method-override");
 const connectDB = require("./config/db");
 const index = require("./routes/index");
+const authroute = require("./routes/auth");
 //load config
 require("dotenv").config({ path: "./config/config.env" });
 
@@ -14,6 +17,9 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+//passport config
+
+require("./config/passport")(passport);
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -23,11 +29,27 @@ app.engine("handlebars", engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
+//sessions
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //static folder
 app.use(express.static("public"));
 
 // routes
 app.use("/", index);
+app.use("/auth", authroute);
+
 const PORT = process.env.PORT || 3001;
 
 connectDB();
