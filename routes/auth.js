1074ab8dc1e033b9a1cc431c822ec5ff2dbe5a32
adil tahiri <rajaadil1995@gmail.com>
2022-08-22 01:00:20 +0100

    add logout

--- routes/auth.js
+++ routes/auth.js
@@ -5,3 +5,23 @@ const router = express.Router();
 // @desc    Auth with Google
 // @route   GET /auth/google
 router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+//@desc google auth callback
+//route GET /auth/google/callback
+
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }, (req, res) => {
+    res.redirect("/dashboard");
+  })
+);
+
+//@desc logout User
+//@router /auth/logout
+
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
