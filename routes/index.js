81cb5d1e4bda73da3e0d120c9e84dc4b39849614
adil tahiri <rajaadil1995@gmail.com>
2022-08-22 01:02:16 +0100

    add authenticated to dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const Story = require("../models/Story");
 
 //@desc  login Landing Page
 //@routes   GET /login
 
-router.get("/", (req, res) => {
+router.get("/", ensureGuest, async (req, res) => {
   res.render("login", { layout: "login" });
 });
 //@desc   dashboard Landing Page
 //@routes   GET /Dashboard
 
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard");
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id });
+    res.render("dashboard", { name: req.user.firstName, stories });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
 });
 
 module.exports = router;
